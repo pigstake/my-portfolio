db1993014254e3953b960b3277ab52eaafd160e5
Jamie Caloras <pigstake@users.noreply.github.com>
2020-10-06 18:31:45 -0400

    remove vital and old work, rewords a few things, add beginning of TpT, add snippet vs story headings

--- src/App.js
+++ src/App.js
@@ -7,30 +7,37 @@ class App extends Component {
     return (
       <main>
         <header className='hero'>
-          <p className='subtitle'>Hi, I’m <a href='#aboutMe'>Jamie.</a> I’ve been designing for humans and our screens since 2010. Below you’ll find artifacts, examples, and stories of my work. You’ll find aspects of me on <a href='https://twitter.com/pigstake'>Twitter</a>, <a href="https://medium.com/@pigstake">Medium</a>, <a href="https://www.instagram.com/pigstake">Instagram</a>, and <a href="https://www.linkedin.com/in/jamisoncaloras/">LinkedIn</a>.</p>
+          <p className='subtitle'>Hi, I’m <a href='#aboutMe'>Jamie.</a> I’ve been designing (mostly for screens) since 2010 — leading startups through research and strategy, finding joy in craft and code, and mentoring peers and newbies. Below you’ll find snippets and stories of my work. Find more on <a href='https://twitter.com/pigstake'>Twitter</a>, <a href="https://medium.com/@pigstake">Medium</a>, and <a href="https://www.linkedin.com/in/jamisoncaloras/">LinkedIn</a>.</p>
         </header>
         <article className='work'>
           <section className='work-copy'>
-            <h2>Recent Work</h2>
-            <p>Since 2017, I’ve worked as a cofounder and lead product design contributor on startup teams in fashion, ecommerce, and finance.</p>
-            <h3>Guai Goods</h3>
-            <h5>Helping you find secondhand and slow fashion clothes and inspiration <br /> <a href='http://guaigoods.com'>Visit Guai Goods</a></h5>
-            <p>The fashion industry is toxic – <a href='https://www.greenpeace.org/archive-international/en/publications/Campaign-reports/Toxics-reports/After-the-Binge-the-Hangover/'>for you</a>, <a href='https://www.theguardian.com/teacher-network/teacher-blog/2013/may/19/fashion-industry-garment-factory-collapse-teaching-news-resources'>for the people who make your clothes</a>, <a href='https://www.vice.com/en_us/article/437egg/why-fashion-is-the-worlds-most-polluting-industry'>for the environment</a>. If you prefer watching to reading, watch the <a href='https://medium.com/@didem.demirci/overconsumption-in-fashion-industry-3cd58ec84020'>TED talks linked in this Medium piece</a>. My wife and I aim to solve clothing problems with our love of secondhand and slow fashion. Right now we publish a weekly newsletter meant to inspire, educate, and provide a healthier shopping outlet. Take a sneak peak at items we curate on <a href="https://www.instagram.com/guaigoods/">Instagram @guaigoods</a>.</p>
-            <h3>Shogun</h3>
-            <h5>High-impact features for the most popular Shopify page builder <br/> <a href='http://www.getshogun.com'>Visit Shogun</a></h5>
-            <p>When your team quadruples in size in a year, <a href='https://norrisnode.com/3-inflection-points/'>expect growing pains</a>. I helped engineers better grok designs with standardized UI patterns. We put the patterns to work building Shogun’s long overdue responsive editing tools and custom form builder.</p>
-            <img className='img-full' src='portfolio/shogun-responsive.gif' alt='Animated gif of Shogun responsive editor'/>
-            <h3>Kindur</h3>
-            <h5>Retirement finance advice  and a living style guide <br/> <a href='http://www.kindur.com'>Visit Kindur</a></h5>
-            <p>My role at Kindur was working with engineers to unpack the practical realities of the founder’s vision as we built and evaluated the product. Day-to-day included lots of white board sketching with teammates, exploring data visualization options, shaping a loose brand guide into a UI system, and coding in React.</p>
-            <img className='img-full' src='portfolio/kindur.png' alt='Kindur screenshots'/>
-            <h3>Vital Card</h3>
-            <h5>Experiment-driven design to optimize acquisition and referrals <br/> <a href='http://bit.ly/vital-card'>Visit Vital</a></h5>
-            <p>My friend and old coworker recruited me onto the team. He and I established a process in which we could iterate and optimize as quickly as possible over six months. Along with the founder, a data scientist, and two engineers, I ran two-week design and product experiments to optimize sign-ups and referral rate.</p>
-            <img className='img-full' src='portfolio/vital-screenshot--wide.png' alt='VITAL landing page'/>
+            <h4>Recent Work Snippets</h4>
+            <div className='work-snippet'>
+              <h3>Teachers Pay Teachers</h3>
+              <h5>**something something** <br /> <a href='https://www.teacherspayteachers.com/TpTSchoolAccess/Teachers'>Visit School Access</a></h5>
+              <p>T</p>
+            </div>
+            <div className='work-snippet'>
+              <h3>Guai Goods</h3>
+              <h5>Helping you find secondhand and slow fashion clothes and inspiration <br /> <a href='http://guaigoods.com'>Visit Guai Goods</a></h5>
+              <p>The fashion industry is toxic – <a href='https://www.greenpeace.org/archive-international/en/publications/Campaign-reports/Toxics-reports/After-the-Binge-the-Hangover/'>for you</a>, <a href='https://www.theguardian.com/teacher-network/teacher-blog/2013/may/19/fashion-industry-garment-factory-collapse-teaching-news-resources'>for the people who make your clothes</a>, <a href='https://www.vice.com/en_us/article/437egg/why-fashion-is-the-worlds-most-polluting-industry'>for the environment</a>. If you prefer watching to reading, watch the <a href='https://medium.com/@didem.demirci/overconsumption-in-fashion-industry-3cd58ec84020'>TED talks linked in this Medium piece</a>. My wife and I aim to solve clothing problems with our love of secondhand and slow fashion. Right now we publish a weekly newsletter meant to inspire, educate, and provide a healthier shopping outlet. Take a sneak peak at items we curate on <a href="https://www.instagram.com/guaigoods/">Instagram @guaigoods</a>.</p>
+            </div>
+            <div className='work-snippet'>
+              <h3>Shogun</h3>
+              <h5>High-impact features for the most popular Shopify page builder <br/> <a href='http://www.getshogun.com'>Visit Shogun</a></h5>
+              <p>When your team quadruples in size in a year, <a href='https://norrisnode.com/3-inflection-points/'>expect growing pains</a>. I helped engineers better grok designs with standardized UI patterns. We put the patterns to work building Shogun’s long overdue responsive editing tools and custom form builder.</p>
+              <img className='img-full' src='portfolio/shogun-responsive.gif' alt='Animated gif of Shogun responsive editor'/>
+            </div>
+            <div className='work-snippet'>
+              <h3>Kindur</h3>
+              <h5>Retirement finance advice  and a living style guide <br/> <a href='http://www.kindur.com'>Visit Kindur</a></h5>
+              <p>My role at Kindur was working with engineers to unpack the practical realities of the founder’s vision as we built and evaluated the product. Day-to-day included lots of white board sketching with teammates, exploring data visualization options, shaping a loose brand guide into a UI system, and coding in React.</p>
+              <img className='img-full' src='portfolio/kindur.png' alt='Kindur screenshots'/>
+            </div>
           </section>
         </article>
         <article className='work'>
+          <h4>Long(ish) Reads</h4>
           <section className='work-copy'>
             <header className='work-header'>
               <h2>Dotvoter</h2>
@@ -71,7 +78,7 @@ class App extends Component {
               <h2>Badger</h2>
               <h5>Visualizing work data to help distributed sales teams work smarter and happier <br/> <a href='http://bit.ly/pwc-badger-google-photos'>Browse design artifacts</a></h5>
             </header>
-            <p>Badger is a performance feedback mobile app. How can personal data help remote salespeople? Their teams? Their managers? And how could my team alleviate the mistrust in how managers could use data? I led design on this product at Philosophie. My team was five strong — three engineers, myself, and a product manager.</p>
+            <p>Badger is a performance feedback mobile app. How can personal data help remote salespeople? Their teams? Their managers? And how could my team alleviate the mistrust in how managers could use data? I led design on this product at Philosophie. We were a team of five — three engineers, myself, and a product manager.</p>
           </section>
           <figure className='fullFigure'>
             <img className='img-full' src='portfolio/badger-screenshot.png' alt='Badger app'/>
@@ -96,7 +103,7 @@ class App extends Component {
               <h2>Field Service OS</h2>
               <h5>A friendly robot and new way of working to connect a field service team <br/> <a href='http://bit.ly/pwc-fso-google-photos'>Browse design artifacts</a></h5>
             </header>
-            <p>How might a band of techies help a 70-year old service company better serve their customers? My team was six strong — one strategist, two designers, two engineers, and a program manager. I led the team through <a href='http://bit.ly/organizing-ux-research'>field research</a>, cooperative design workshops, and prototype tests with dispatchers, warehouse managers, and field technicians to discover how we could help.</p>
+            <p>How might a band of techies help a 70-year old service company better serve their customers? We were a team of six — one strategist, two designers, two engineers, and a program manager. I led the team through <a href='http://bit.ly/organizing-ux-research'>field research</a>, cooperative design workshops, and prototype tests with dispatchers, warehouse managers, and field technicians to discover how we could help.</p>
           </section>
           <figure className='fullFigure'>
             <img className='img-full' src='portfolio/fso-screenshot.png' alt='FSO app'/>
@@ -138,28 +145,11 @@ class App extends Component {
             <p>This is not a software startup; it’s a pizza restaurant. <a href='https://tcrn.ch/2xVBev7'>All companies might be software companies</a> in the future. I helped the founder see value in digital extensions of his service, and the work it takes to build the right things — teaching and working alongside him with design collaboration, agile implementation planning, and some favorite tools like Slack, Invision and Trello. You can see the two of us in the middle of a design studio in the above photo.</p>
           </section>
         </article>
-        <article className='work'>
-          <section className='work-copy'>
-            <h2>Older work</h2>
-            <h3>Venmetro</h3>
-            <h5>The first online lending platform in Panama <br/> <a href='http://bit.ly/venmetro'>Visit site</a></h5>
-            <p><strong>My contribution:</strong> UX research, interaction design, UI design, front-end code, brand design</p>
-            <h3>Greenpeace</h3>
-            <h5>The Environmentalist Blog</h5>
-            <p><strong>My contribution:</strong> UI design, front-end code, information architecture</p>
-            <h3 className='m-topOnly'>Washington Interactive Network</h3>
-            <h5>Local Seattle accelerator and coworking space <br/> <a href='http://bit.ly/reactor-win'>Visit site</a></h5>
-            <p><strong>My contribution:</strong> UI design, front-end code, information architecture</p>
-            <h3 className='m-topOnly'>Streamline Alliance</h3>
-            <h5>Online presence for a global manufacturing consultant <br/> <a href='http://bit.ly/streamline-alliance'>Visit site</a></h5>
-            <p><strong>My contribution:</strong> interaction design, UI design, front-end code</p>
-          </section>
-        </article>
         <article className='work' id='aboutMe'>
           <section className='work-copy'>
             <header className='work-header'>
               <h2>About me</h2>
-              <h5>Designer, board game enthusiast, bicycle ogler, and amateur bread baker in Brooklyn</h5>
+              <h5>Designer, board game enthusiast, vintage bicycle romantic, and amateur bread baker in Brooklyn</h5>
             </header>
             <p>Hello! <span role="img" aria-label="wave emoji">👋</span> I’m a problem finder, definer, prioritizer, and solver — a Designer. I moderate warm and hospitable research sessions, facilitate lively creative collaborations, help my teams understand users, build design systems, test prototypes, and ship product. My specialty is user-centered design of digital things.</p>
             <p>Since 2010, I’ve been fortunate to work with wonderful in-house, agency, and consulting teams, solving problems for clients all over the world, contributing to and leading the design work, teaching and (often learning!) as I go. I have particularly enjoyed the times working closely with small, diverse teams full of trust and collaborative rhythm. If you’re reading this — thank you.</p>
